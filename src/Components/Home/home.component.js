56bf36c7756cbe5ffdfeb14693606fe09daa6d3b
ErblinIsaku <53400779+ErblinIsaku@users.noreply.github.com>
2020-01-19 21:20:02 +0100

    Home page
    finished

--- src/Components/Home/home.component.js
+++ src/Components/Home/home.component.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 import { Row, Col, Jumbotron } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import '../Home/home.component.css';
 export class Home extends React.Component {
 
     // constructor() {
@@ -13,21 +14,29 @@ export class Home extends React.Component {
     render() {
         return (
             <Container fluid >
-                <Jumbotron>
-                    <Row>
+                <Jumbotron className='jmb'>
+                <h1 className='busticket'>BUS TICKET</h1>
+                
+                <h3 className="busticket1"> Rezervoni dhe blini online biletën tuaj të autobuzit shumë shpejtë dhe thjeshtë, kudo ku ndodheni brenda Shqipërisë.
+                </h3>
+  
+                    <Row className='buton'>
                         <Col xs={4}></Col>
                         <Col xs={2} >
-                            <Button size='lg' variant="outline-warning"><Link style={{ textDecoration: 'none', color: 'inherit' }} to="/urban"> Urban</Link></Button>
+                            <Button size='lg' className='btn1' variant="warning"><Link style={{ textDecoration: 'none', color: 'inherit' }} to="/urban"> URBAN</Link></Button>
                         </Col>
                         <Col xs={2}>
-                            <Button size='lg' variant="outline-warning"> InterUrban</Button>
+                            <Button size='lg' className='btn1' variant="warning"> INTERURBAN</Button>
                         </Col>
                         <Col xs={4}></Col>
                     </Row>
                 </Jumbotron>
             </Container>
+            
         )
 
     }
+
+    
 }
 
