eeaaa033120e554362269a2dc310e3a7f8ea08ba
Joh4n10 <48352084+Joh4n10@users.noreply.github.com>
2020-01-28 20:20:44 +0100

    about and footer

--- src/Routing/routing.js
+++ src/Routing/routing.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { Home } from '../Components/Home/home.component';
 import { InterUrban } from '../Components/Inter-urban/Inter-urban.component.js';
+import { About } from '../Components/About/about.js';
 
 export function Routing() {
     return (
@@ -10,6 +11,8 @@ export function Routing() {
             <Route exact path="/" component={Home} />
             <Route path="/urban" component={UrbanTickets} />
             <Route path="/inter-urban" component={InterUrban} />
+            <Route path="/about" component={About} />
+
         </Switch>
     );
 }
