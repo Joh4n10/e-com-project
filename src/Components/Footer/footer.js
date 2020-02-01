8a7b7a335bd2c3502504e99432682544d67b4292
Joh4n10 <48352084+Joh4n10@users.noreply.github.com>
2020-02-01 20:57:19 +0100

    fix warnings

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -32,8 +32,8 @@ export class Footer extends React.Component {
                                 <h5>Kontaktet</h5>
                                 <ul className="list-unstyled quick-links">
                                     <li><a href="mailto:support@busticket.al"><i className="fa fa-angle-double-right"></i>support@busticket.al</a></li>
-                                    <li><a><i className="fa fa-angle-double-right"></i>+355 69 000 0000</a></li>
-                                    <li><a><i className="fa fa-angle-double-right"></i>+355 68 111 0000</a></li>
+                                    <li><a href="test"><i className="fa fa-angle-double-right"></i>+355 69 000 0000</a></li>
+                                    <li><a href="test"><i className="fa fa-angle-double-right"></i>+355 68 111 0000</a></li>
 
                                     {/* <li><a href="test"><i className="fa fa-angle-double-right"></i>Home</a></li>
                                     <li><a href="test"><i className="fa fa-angle-double-right"></i>About</a></li>
@@ -45,15 +45,15 @@ export class Footer extends React.Component {
                             <div className="col-xs-12 col-sm-4 col-md-4">
                                 <h5>Partneret</h5>
                                 <ul className="list-unstyled quick-links">
-                                    <li><a><i className="fa fa-angle-double-right"></i>Alb - Trans sh.p.k</a></li>
-                                    <li><a><i className="fa fa-angle-double-right"></i>AlbaTrans</a></li>
-                                    <li><a><i className="fa fa-angle-double-right"></i>Tirana Metropol</a></li>
-                                   
+                                    <li><a href="test"><i className="fa fa-angle-double-right"></i>Alb - Trans sh.p.k</a></li>
+                                    <li><a href="test"><i className="fa fa-angle-double-right"></i>AlbaTrans</a></li>
+                                    <li><a href="test"><i className="fa fa-angle-double-right"></i>Tirana Metropol</a></li>
+
                                 </ul>
                             </div>
                         </div>
                         <div className="row">
-                  
+
                             <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
                                 <ul className="list-unstyled list-inline social text-center">
                                     <li className="list-inline-item"><a href="test"><i className="fa fa-facebook"></i></a></li>
@@ -67,7 +67,7 @@ export class Footer extends React.Component {
                         <div className="row">
                             <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
                                 <p><u><a href="/">busticket.al</a></u> - Projekt i krijuar nga studente te UAMD - FTI per qellime akademike ne kursin E-COMMERCE & E-BUSINESS</p>
-                                <p className="h6">All right Reversed.<a className="text-green ml-2" href="" target="_blank"></a><p>Johan Dhana & Erblin Isaku</p></p>
+                                <p className="h6">All right Reversed.<p>Johan Dhana & Erblin Isaku</p></p>
                             </div>
                         </div>
                     </div>
