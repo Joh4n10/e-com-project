66971a0ad3f44590a56d083dabc0704879acdd7a
Joh4n10 <48352084+Joh4n10@users.noreply.github.com>
2020-02-16 15:46:50 +0100

    change page title

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -19,7 +19,7 @@ export class About extends React.Component {
             <>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
                     <div className="container">
-                        <a className="navbar-brand js-scroll-trigger" href="#page-top"><img alt="as" src={require('../../Assets/bus.png')} style={{ width: "80%" }} /></a>
+                        <a className="navbar-brand js-scroll-trigger" href="/"><img alt="as" src={require('../../Assets/bus.png')} style={{ width: "80%" }} /></a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -65,38 +65,38 @@ export class About extends React.Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-8 mx-auto">
-                                <h2 style= {{textAlign: "center"}}>Sherbimet qe ofrohen</h2>
-<Row>
-                                  <Col lg="6">
-                                <ul>
-                                    <li>Interurban</li>
-                                    <li>Blerje online te biletes</li>
-                                    <li>Oraret e udhëtimeve</li>
-                                    <li>Raporte/Njoftime (mbi linjat e udhëtimit)</li>
-                                    <li>Rezervim të vendit në autobus</li>
-                                    <li>Menaxhim i programit të udhëtimit</li>
-                                    <li>Menaxhim i destinacionit të udhëtimit</li>
-                                    <li>Printim të biletave/rezervimit</li>
+                                <h2 style={{ textAlign: "center" }}>Sherbimet qe ofrohen</h2>
+                                <Row>
+                                    <Col lg="6">
+                                        <ul>
+                                            <li>Interurban</li>
+                                            <li>Blerje online te biletes</li>
+                                            <li>Oraret e udhëtimeve</li>
+                                            <li>Raporte/Njoftime (mbi linjat e udhëtimit)</li>
+                                            <li>Rezervim të vendit në autobus</li>
+                                            <li>Menaxhim i programit të udhëtimit</li>
+                                            <li>Menaxhim i destinacionit të udhëtimit</li>
+                                            <li>Printim të biletave/rezervimit</li>
 
-                                </ul>
-                                </Col>
-                                <Col lg="6">
-                                <ul>
-                                    <li>Urban</li>
-                                    <li>Abone Online</li>
-                                    <li>Linjat e udhëtimit të transportit publik</li>
-                                    <li>Intervalet e linjave per tranpsortin qytetas</li>
-                                    <li>Informacion mbi firmat operuese</li>
-                                    <li>Ndarje te cmimit sipas kategorive</li>
-                                    <li>Raporte/Njoftime (mbi linjat e udhëtimit)</li>
-                                    <li>Monitorim live te vendodhjes se autobusit</li>
+                                        </ul>
+                                    </Col>
+                                    <Col lg="6">
+                                        <ul>
+                                            <li>Urban</li>
+                                            <li>Abone Online</li>
+                                            <li>Linjat e udhëtimit të transportit publik</li>
+                                            <li>Intervalet e linjave per tranpsortin qytetas</li>
+                                            <li>Informacion mbi firmat operuese</li>
+                                            <li>Ndarje te cmimit sipas kategorive</li>
+                                            <li>Raporte/Njoftime (mbi linjat e udhëtimit)</li>
+                                            <li>Monitorim live te vendodhjes se autobusit</li>
 
-                                </ul>
+                                        </ul>
 
-                                </Col>
+                                    </Col>
 
                                 </Row>
-                                
+
                                 <h6 className="lead1">Disa nga sherbimet e ofruara ende jane te pa perfunduara/realizuara</h6>
 
                             </div>
@@ -108,7 +108,7 @@ export class About extends React.Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-8 mx-auto">
-                                <h2 style = {{textAlign: "center"}}>Kontakto</h2>
+                                <h2 style={{ textAlign: "center" }}>Kontakto</h2>
                                 {/* <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
                                 <ul className="list-unstyled list-inline social text-center">
                                     <li className="list-inline-item"><a href="test"><i className="fa fa-facebook"></i></a></li>
@@ -121,41 +121,41 @@ export class About extends React.Component {
                             </div>
                         </div>
                     </div>
-                                    </section>
+                </section>
                 <Container>
-                <Row>
-                <Col lg="2"></Col>
-                <Col lg="8"> <Form>
-  <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>Email</Form.Label>
-    <Form.Control type="email" placeholder="emri@shembull.com" />
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlSelect1">
-    <Form.Label>Qyteti</Form.Label>
-    <Form.Control as="select">
-      <option>Tirane</option>
-      <option>Durres</option>
-      <option>Berat</option>
-      <option>Vlore</option>
-      <option>Shkoder</option>
-    </Form.Control>
-  </Form.Group>
-   <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Mesazhi</Form.Label>
-    <Form.Control as="textarea" rows="3" />
-  </Form.Group>
-</Form>
-<Button variant="primary" type="submit">
-    Dergo
+                    <Row>
+                        <Col lg="2"></Col>
+                        <Col lg="8"> <Form>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="emri@shembull.com" />
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Qyteti</Form.Label>
+                                <Form.Control as="select">
+                                    <option>Tirane</option>
+                                    <option>Durres</option>
+                                    <option>Berat</option>
+                                    <option>Vlore</option>
+                                    <option>Shkoder</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Mesazhi</Form.Label>
+                                <Form.Control as="textarea" rows="3" />
+                            </Form.Group>
+                        </Form>
+                            <Button variant="primary" type="submit">
+                                Dergo
   </Button></Col>
-                <Col lg="2"></Col>
-                
-                </Row>
-               <p className='p'></p>
-               
+                        <Col lg="2"></Col>
 
-                
-               
+                    </Row>
+                    <p className='p'></p>
+
+
+
+
                 </Container>
             </>
         )
